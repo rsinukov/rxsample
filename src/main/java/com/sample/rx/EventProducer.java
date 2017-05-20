7e95e929000ab26f572361235dbf35b87d0136a4
Rustam Sinukov <rustik1@yandex-team.ru>
2017-05-20 23:06:42 +0300

    Move params to config

--- src/main/java/com/sample/rx/EventProducer.java
+++ src/main/java/com/sample/rx/EventProducer.java
@@ -1,5 +1,6 @@
 package com.sample.rx;
 
+import com.google.auto.value.AutoValue;
 import rx.Observable;
 import rx.Scheduler;
 
@@ -7,17 +8,41 @@ import java.util.concurrent.TimeUnit;
 
 public class EventProducer {
 
-    public Observable<String> events(
-            long periodMillis,
-            long delayMillis,
-            Scheduler periodScheduler,
-            Scheduler delayScheduler,
-            int shareCount
-    ) {
-        return Observable.interval(periodMillis, TimeUnit.MILLISECONDS, periodScheduler)
+    private final Config config;
+
+    public EventProducer(Config config) {
+        this.config = config;
+    }
+
+    public Observable<String> events() {
+        return Observable.interval(config.periodMillis(), TimeUnit.MILLISECONDS, config.periodScheduler())
                 .map(String::valueOf)
-                .delay(delayMillis, TimeUnit.MILLISECONDS, delayScheduler)
+                .delay(config.delayMillis(), TimeUnit.MILLISECONDS, config.delayScheduler())
                 .replay()
-                .autoConnect(shareCount);
+                .autoConnect(config.shareCount());
+    }
+
+
+    @AutoValue
+    public abstract static class Config {
+        abstract long periodMillis();
+        abstract long delayMillis();
+        abstract int shareCount();
+        abstract Scheduler periodScheduler();
+        abstract Scheduler delayScheduler();
+
+        public static Builder builder() {
+            return new AutoValue_EventProducer_Config.Builder();
+        }
+
+        @AutoValue.Builder
+        public abstract static class Builder {
+            public abstract Builder periodMillis(long periodMillis);
+            public abstract Builder delayMillis(long delayMillis);
+            public abstract Builder shareCount(int shareCount);
+            public abstract Builder periodScheduler(Scheduler periodScheduler);
+            public abstract Builder delayScheduler(Scheduler delayScheduler);
+            public abstract Config build();
+        }
     }
 }
--- src/main/java/com/sample/rx/RxSample.java
+++ src/main/java/com/sample/rx/RxSample.java
@@ -6,12 +6,20 @@ import rx.schedulers.Schedulers;
 public class RxSample {
 
     public static void main(String args[]) {
-        final EventProducer producer = new EventProducer();
+        final EventProducer.Config config = EventProducer.Config.builder()
+                .periodMillis(100)
+                .delayMillis(200)
+                .periodScheduler(Schedulers.immediate())
+                .delayScheduler(Schedulers.immediate())
+                .shareCount(3)
+                .build();
 
-        final Observable<String> sharedObservable = producer.events(100, 200, Schedulers.immediate(), Schedulers.immediate(), 3);
+        final EventProducer producer = new EventProducer(config);
 
-        sharedObservable.subscribe(item -> System.out.println("First subscriber: " + item));
-        sharedObservable.subscribe(item -> System.out.println("Second subscriber: " + item));
-        sharedObservable.subscribe(item -> System.out.println("Third subscriber: " + item));
+        final Observable<String> sharedObservable = producer.events();
+
+        sharedObservable.subscribe(item -> System.out.println("First subscriber: " + item + " " + System.currentTimeMillis()));
+        sharedObservable.subscribe(item -> System.out.println("Second subscriber: " + item + " " + System.currentTimeMillis()));
+        sharedObservable.subscribe(item -> System.out.println("Third subscriber: " + item + " " + System.currentTimeMillis()));
     }
 }
