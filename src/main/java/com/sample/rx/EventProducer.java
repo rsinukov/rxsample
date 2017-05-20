1fe0199ac3061e8068bef00e5d2de0e1ae86a168
Rustam Sinukov <rustik1@yandex-team.ru>
2017-05-20 22:50:30 +0300

    Share observable

--- src/main/java/com/sample/rx/EventProducer.java
+++ src/main/java/com/sample/rx/EventProducer.java
@@ -7,8 +7,17 @@ import java.util.concurrent.TimeUnit;
 
 public class EventProducer {
 
-    public Observable<String> events(long periodMillis, Scheduler timeScheduler) {
-        return Observable.interval(periodMillis, TimeUnit.MILLISECONDS, timeScheduler)
-                .map(String::valueOf);
+    public Observable<String> events(
+            long periodMillis,
+            long delayMillis,
+            Scheduler periodScheduler,
+            Scheduler delayScheduler,
+            int shareCount
+    ) {
+        return Observable.interval(periodMillis, TimeUnit.MILLISECONDS, periodScheduler)
+                .map(String::valueOf)
+                .delay(delayMillis, TimeUnit.MILLISECONDS, delayScheduler)
+                .replay()
+                .autoConnect(shareCount);
     }
 }
--- src/main/java/com/sample/rx/RxSample.java
+++ src/main/java/com/sample/rx/RxSample.java
@@ -1,5 +1,6 @@
 package com.sample.rx;
 
+import rx.Observable;
 import rx.schedulers.Schedulers;
 
 public class RxSample {
@@ -7,7 +8,10 @@ public class RxSample {
     public static void main(String args[]) {
         final EventProducer producer = new EventProducer();
 
-        producer.events(100, Schedulers.immediate())
-                .subscribe(item -> System.out.println(item));
+        final Observable<String> sharedObservable = producer.events(100, 200, Schedulers.immediate(), Schedulers.immediate(), 3);
+
+        sharedObservable.subscribe(item -> System.out.println("First subscriber: " + item));
+        sharedObservable.subscribe(item -> System.out.println("Second subscriber: " + item));
+        sharedObservable.subscribe(item -> System.out.println("Third subscriber: " + item));
     }
 }
